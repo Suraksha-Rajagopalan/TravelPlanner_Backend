821106e627950063a1e4a9633da5767412f70a0b
Suraksha Rajagopalan <surakshagopalan@gmail.com>
2025-06-09 21:57:10 +0530

    Add files via upload

---
+++ TravelPlannerAPI/Models/EmailRequest.cs
@@ -0,0 +1,8 @@
+﻿public class EmailRequest
+{
+    public string AccessToken { get; set; }
+    public string FromEmail { get; set; }
+    public string ToEmail { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
